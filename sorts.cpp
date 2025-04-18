3747b6261ff5d320b41c811af0b0da75ef6ebe79
Jonah R Draves <36460022+Jonah-Draves@users.noreply.github.com>
2025-04-18 19:45:04 -0400

    fix: fixed quicksort, taking median pivot was bugging the logic

--- sorts.cpp
+++ sorts.cpp
@@ -7,8 +7,7 @@ using namespace std;
 
 //The code for quicksort was based off of the Module 9 UFOnline lecture code and modified for use in this project
 int partition(vector<pair<string, int>>& sList, int low, int high) {
-    int median = (low + high) / 2;
-    int pivot = sList[median].second;
+    int pivot = sList[low].second;
     int up = low, down = high;
     while (up < down) {
         for (int j = up; j < high; j++) {
